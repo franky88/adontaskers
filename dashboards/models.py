ee468d075114f4c67c6d0382c71053943f7ccbd7
Frank Ramos <ramosfp99@gmail.com>
2022-07-21 10:46:40 +0800

    updates

--- dashboards/models.py
+++ dashboards/models.py
@@ -1,7 +1,9 @@
 from statistics import mode
+from tabnanny import verbose
 from django.db import models
 from django.contrib.auth.models import User
 from django.contrib.auth.signals import user_logged_in
+from django.db.models.signals import post_save, pre_save
 from django.dispatch import receiver
 from django.utils import timezone
 # Create your models here.
@@ -9,6 +11,8 @@ class UserLogs(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     time_in = models.DateTimeField(default=timezone.now)
     time_out = models.DateTimeField(null=True, blank=True)
+    class Meta:
+        verbose_name_plural = 'user logs'
     def __Str__(self):
         return self.time_in, self.time_out
 
@@ -25,5 +29,13 @@ class TopDesigner(models.Model):
 def post_login(sender, request, user, **kwargs):
     print(sender)
     print(f'User: {user.username} Logged in' )
-    user.time_in = timezone.now()
-    user.save()
+    print(user_logged_in)
+    if user_logged_in:
+        print("User login ")
+        user.time_in = timezone.now()
+        user.save()
+
+@receiver(pre_save, sender=User)
+def pre_save_time_log(sender, instance, *args, **kwargs):
+    if user_logged_in:
+         print("User login sample")
